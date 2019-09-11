8fb49c4de94c495e5ffc43397d5b0b9984d0edbb
Iamniloy <sudipta980@gmail.com>
2019-09-11 15:39:51 -0400

    removed errors from the code

--- week1Challenge/src/week1Challenge/MinimumMutations.java
+++ week1Challenge/src/week1Challenge/MinimumMutations.java
@@ -14,8 +14,7 @@ public class MinimumMutations {
 		//System.out.println(start.equals(end));
 	}
 
-<<<<<<< HEAD
-=======
+
 }
 
 class FindingMinimumMutations{
@@ -51,40 +50,5 @@ class FindingMinimumMutations{
 		return 0;
 	}
 
->>>>>>> 9a738203e7cd21c86b77cd9e8908c8df0ea83099
-}
-
-class FindingMinimumMutations{
-	int findMutation(String start, String end, String bank[]) {
-		if(!(start.equals(end))) {
-			ArrayList<String> mutations = new ArrayList<String>();
-			StringBuilder startMutation = new StringBuilder(start);
-			int count=0;
-			int checker=0;
-			//System.out.println(count);
-			for(int i = 0; i<8; i++) {
-				if(start.charAt(i)!=end.charAt(i)) {
-					startMutation.setCharAt(i, end.charAt(i));
-					mutations.add(startMutation.toString());
-					count++;
-				}
-			}
-			//System.out.println(count);
-			//System.out.println(mutations);
-
-			for(int i =0; i<mutations.size(); i++) {
-				for(int j = 0; j < bank.length; j++)
-					if(mutations.get(i).equals(bank[j])) {
-						checker++;
-					}
-			}
-			//System.out.println(checker);
-			if(checker==count) {
-				return count;
-			}
-
-		}
-		return 0;
-	}
 }
 
